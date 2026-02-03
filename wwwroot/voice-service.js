35f1a67d25e3143610bf9505c508886ef48d13ae
Julien Cousineau <julien@Mac.localdomain>
2026-02-02 20:01:24 -0500

    fix: Improve emoji removal to prevent TTS saying 'stopwatch'
    Expanded emoji regex range to catch all emojis including ⏱️ (stopwatch).
    Now TTS will say '20 seconds' instead of 'stopwatch 20 seconds'.

--- wwwroot/voice-service.js
+++ wwwroot/voice-service.js
@@ -319,8 +319,8 @@ class VoiceService {
      */
     cleanTextForSpeech(text) {
         return text
-            // Remove emojis
-            .replace(/[\u{1F300}-\u{1F9FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|[\u{1F600}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]/gu, '')
+            // Remove all emojis comprehensively
+            .replace(/[\u{1F000}-\u{1F9FF}]|[\u{2600}-\u{27BF}]|[\u{E000}-\u{F8FF}]|[\u{FE00}-\u{FE0F}]|[\u{1F300}-\u{1FFFF}]/gu, '')
             // Remove markdown bold/italic
             .replace(/\*\*(.*?)\*\*/g, '$1')
             .replace(/\*(.*?)\*/g, '$1')
