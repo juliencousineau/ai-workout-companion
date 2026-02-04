dc405f65b567a743f031d0e2fe80c695ec9c16dc
Julien Cousineau <cousineau.julien@gmail.com>
2026-02-04 08:09:35 -0500

    fix: Add DataProtectionKeys to model snapshot (#21)
    Co-authored-by: Julien Cousineau <julien@Mac.localdomain>

--- Migrations/AppDbContextModelSnapshot.cs
+++ Migrations/AppDbContextModelSnapshot.cs
@@ -22,6 +22,25 @@ namespace aiworkoutcompanion.Migrations
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
 
+            modelBuilder.Entity("Microsoft.AspNetCore.DataProtection.EntityFrameworkCore.DataProtectionKey", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("integer");
+
+                    NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("FriendlyName")
+                        .HasColumnType("text");
+
+                    b.Property<string>("Xml")
+                        .HasColumnType("text");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("DataProtectionKeys");
+                });
+
             modelBuilder.Entity("AiWorkoutCompanion.Data.UserCredential", b =>
                 {
                     b.Property<int>("Id")
